511811753e45c4b7ecb74ddcface4a3b14d7a2b8
Martin Jones <martin@martinjones.info>
2015-01-19 19:36:58 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Getting-and-Cleaning-Data-Project
-Getting-and-Cleaning-Data-Project - Unit 3 of the Coursera / John Hopkins Data Science Course
+# Getting and Cleaning Data
+
+Martin Jones
+
+## Course Project
+
+An R script called run_analysis.R that does the following:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive activity names.
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## Steps to use this program
+
+1. Place run_analysis.R in a working directory.
+
+2. From the same directory where you placed run_analysis.R run the following commands to download the source data in to a data subdirectory and unzip it:
+
+if(!file.exists("./data")){
+        dir.create("./data")}
+dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(dataUrl,destfile="./data/Dataset.zip",method="curl")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+3. Run run_analysis.R  to generate a new file tiny_data.txt in your working directory.
+
+
+## run_analysis.R steps explained
+
+* Read all the test, training, subject, activity and features files:
+	X_train.txt
+	y_train.txt
+	subject_train.txt
+	y_test.txt
+	X_test.txt
+	activity_labels.txt
+	features.txt
+
+* Combine the files to a data frame called all_combined in the form of subjects, labels, the rest of the data.
+
+* Apply the features and activity labels
+
+* Extract the mean and standard deviation data in to a new data frame called combinedAverages
+
+* Write the new tidy set into a text file called tidy.txt in the current working directory
+
+### Additional Information
+The tidy.txt data set can be read back in to R with the following command:
+ data <- read.table("tidt.txt", header = TRUE)
+
+You can find additional information about the variables, data and transformations in the CodeBook.MD file.
+
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+##########################################################################################################
+
+## Coursera Getting and Cleaning Data Course Project
+#
+# run_analysis.R   Martin Jones v.1.0  Jan 20th 2015
+#
+# See README.md for instructions on how to run this script.
+# This script will perform the following steps on the UCI HAR Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip which should be unzipped into a subdirectory called data
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive activity names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+##########################################################################################################
+
+#  Read the training and the test data 
+  x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+  y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+  subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+  x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+  y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+  subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+
+#  Read activity labels
+  activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+#  Read features labels
+  features <- read.table("./UCI HAR Dataset/features.txt")
+
+#  Combine the training and test data 
+  x_combined <- rbind(x_train, x_test)
+  y_combined <- rbind(y_train, y_test)
+  s_combined <- rbind(subject_train, subject_test)
+
+# Apply labels to columns
+  names(features) <- c('feat_id', 'feat_name')
+
+# Find the mean or standard deviation     
+  data_mean_std <- grep("-mean\\(\\)|-std\\(\\)", features$feat_name) 
+  x_combined <- x_combined[, data_mean_std] 
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+  names(x_combined) <- gsub("\\(|\\)", "", (features[data_mean_std, 2]))
+
+  names(activities) <- c('act_id', 'act_name')
+  y_combined[, 1] = activities[y_combined[, 1], 2]
+
+  names(y_combined) <- "Activity"
+  names(s_combined) <- "Subject"
+
+  all_combined <- cbind(s_combined, y_combined, x_combined)
+
+#  Create a tidy data set with the average of each variable for each activity and each subject
+  combinedAverages <- aggregate((all_combined[, 3:dim(all_combined)[2]]),list(all_combined$Subject, all_combined$Activity), mean)
+  
+#  Change column names to be more descriptive (see Codebook.md)
+  names(combinedAverages)[1] <- "Subject"
+  names(combinedAverages)[2] <- "Activity"
+  names(combinedAverages) <- gsub("tBodyAcc", "Time.BodyAcc", names(combinedAverages))
+  names(combinedAverages) <- gsub("tGravity", "Time.Gravity", names(combinedAverages))
+  names(combinedAverages) <- gsub("fBody", "FFT.Body", names(combinedAverages))
+  names(combinedAverages) <- gsub("fGravity", "FFT.Gravity", names(combinedAverages))
+  names(combinedAverages) <- gsub("\\-mean\\(\\)\\-", ".Mean.", names(combinedAverages))
+  names(combinedAverages) <- gsub("\\-std\\(\\)\\-", ".Std.", names(combinedAverages))
+  names(combinedAverages) <- gsub("\\-mean\\(\\)", ".Mean", names(combinedAverages))
+  names(combinedAverages) <- gsub("\\-std\\(\\)", ".Std", names(combinedAverages))
+
+# Write tidy data set to a file called tidy.txt in the current directory
+  write.table(combinedAverages, "./tidy.txt", row.names=FALSE)
+
+
+
